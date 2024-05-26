d1a7115e1a4bb75b9d8c2de76afc3b96d322d234
Jose De La Cruz <jldlcv@gmail.com>
2024-05-26 07:58:29 +0200

    Init commit

---
+++ task.py
@@ -0,0 +1,16 @@
+class Tasks:
+	def __init__(self):
+		self.pending = []
+		self.completed = []
+	
+	def add_task(self):
+		pass
+
+	def show_task(self):
+		pass
+
+	def complete_task(self):
+		pass
+
+	def delete_task(self):
+		pass
